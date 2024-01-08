a51a351cb538ce668e82701dfd5ca9f49fc758bb
SzolosyTibor <91724564+SzolosyTibor@users.noreply.github.com>
2024-01-08 21:12:24 +0100

    asd.py

--- asd.py
+++ asd.py
@@ -1,1 +1,9 @@
+import yfinance as yf
+import streamlit as st
 
+tickerSymbol = 'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
